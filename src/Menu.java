e6263e1fc8107f4ff19082b29a95e61f70c478a8
hohohosn <day7of01@gmail.com>
2025-05-01 14:01:37 +0900

    "Menu 클래스 추가"

---
+++ src/Menu.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Menu {
+    private String name;
+    private List<MenuItem> items;
+
+    public Menu(String name) {
+        this.name = name;
+        this.items = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public void printMenuItems() {
+        for (int i = 0; i < items.size(); i++) {
+            MenuItem item = items.get(i);
+            System.out.printf("%d. %s | W %.1f | %s\n", i + 1, item.getName(), item.getPrice(), item.getDescription());
+        }
+        System.out.println("0. 뒤로가기");
+    }
+}
