b6fdcc73d92ecd7c64bf8a76931e72dba4ce72dd
hohohosn <day7of01@gmail.com>
2025-05-01 14:13:10 +0900

    "menu 정리"

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Burger 메뉴
+        Menu burgers = new Menu("Burgers");
+        burgers.addItem(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgers.addItem(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgers.addItem(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.addItem(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        // MenuItem 목록 생성
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        // 음료
 
-        // Kiosk 클래스 사용하여 프로그램 실행
-        Kiosk kiosk = new Kiosk(menuItems);
+        // 디저트
+
+        List<Menu> menus = new ArrayList<>();
+        menus.add(burgers);
+
+        Kiosk kiosk = new Kiosk(menus);
         kiosk.start();
     }
 }
